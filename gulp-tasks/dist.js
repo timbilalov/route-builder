a4f5b1cdb012605e7951581765f6cf2e77c42d1c
unfriend <t-moore@mail.ru>
2019-03-05 19:05:34 +0300

    TEMP: VoiceInput2 disabled

--- gulp-tasks/dist.js
+++ gulp-tasks/dist.js
@@ -1,11 +1,9 @@
 import gulp from 'gulp';
 import gulpif from 'gulp-if';
-import del from 'del';
 
 import { PRODUCTION } from '../config';
 import PATHS from '../paths';
 
 export default function dist() {
-	del(PATHS.dist.to);
 	return gulp.src(PATHS.dist.from).pipe(gulpif(PRODUCTION, gulp.dest(PATHS.dist.to)));
 }
